3027a97632b138576c7bcd1e51838faa83b975ea
Sean Larkin <sean.larkin@cuw.edu>
2023-01-23 19:35:26 +0000

    add tests to fully cover scenarios

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,98 @@ fn _has_flag<I: Iterator<Item = String>>(mut args: I, flag: &str) -> bool {
 
     let position = args.position(|arg| arg == format!("{}{}", prefix, flag));
     let terminator_position = args.position(|arg| arg == "--");
-    position.is_some() && (!terminator_position.is_some() || position < terminator_position)
+
+    position.is_some()
+        && (terminator_position.is_none()
+            || arg_is_before_terminator(position, terminator_position))
+}
+
+fn arg_is_before_terminator(position: Option<usize>, terminator_position: Option<usize>) -> bool {
+    position.is_some()
+        && terminator_position.is_some()
+        && position.unwrap() < terminator_position.unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn args_with_value_not_matching_double_dash() {
+        let args = vec!["--foo", "--unicorn=rainbow", "--bar"];
+        let expected_value = "unicorn=rainbow";
+
+        assert!(_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_matching_value_before_terminator() {
+        let args = vec!["--unicorn", "--", "--foo"];
+        let expected_value = "unicorn";
+
+        assert!(_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_not_matching_value_after_terminator() {
+        let args = vec!["--foo", "--", "--unicorn"];
+        let expected_value = "unicorn";
+
+        assert!(!_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_not_matching_value_not_in_args() {
+        let args = vec!["--foo"];
+        let expected_value = "unicorn";
+
+        assert!(!_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_with_sequential_single_dash_input() {
+        let args = vec!["-f", "-u", "-b"];
+        let expected_value = "-u";
+
+        assert!(_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_with_sequential_single_dash_and_match_before_terminator() {
+        let args = vec!["-u", "--", "-f"];
+        let expected_value = "-u";
+
+        assert!(_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
+    #[test]
+    fn args_with_sequential_single_dash_and_no_match_after_terminator() {
+        let args = vec!["-u", "--", "-f"];
+        let expected_value = "-f";
+
+        assert!(!_has_flag(
+            args.into_iter().map(ToString::to_string),
+            expected_value
+        ))
+    }
+
     #[test]
     fn args_without_double_dash_input() {
         assert!(_has_flag(
